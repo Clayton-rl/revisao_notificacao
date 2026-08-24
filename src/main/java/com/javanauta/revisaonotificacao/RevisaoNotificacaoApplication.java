0c0ed3c4b1085a629d2689aefc0216cf81525031
Clayton <claytonprogrammer@gmail.com>
2026-08-24 09:18:44 -0300

    git inicial com esqueleto

---
+++ src/main/java/com/javanauta/revisaonotificacao/RevisaoNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.revisaonotificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisaoNotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisaoNotificacaoApplication.class, args);
+	}
+
+}
